50d8e67972263405e260b91046598de48da15e70
necmi <necmierturk@gmail.com>
2025-11-12 16:54:31 +0300

    firebase auth

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,4 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+export const login = createAsyncThunk(
+  "user/login",
+  async ({ email, password }) => {
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      const token = user.stsTokenManager.accessToken;
+      const userData = { token, user: user };
+      return userData;
+    } catch (error) {
+      console.log("userSlice 21 line:", error);
+      throw error;
+    }
+  }
+);
 
 const initialState = {
   email: null,
@@ -25,16 +47,33 @@ export const userSlice = createSlice({
       state.isLoading = action.payload;
     },
     setLogin: (state, action) => {
-      if(state.email === state.User.userEmail && state.password === state.User.userPassword){
+      if (
+        state.email === state.User.userEmail &&
+        state.password === state.User.userPassword
+      ) {
         state.isAuth = true;
       } else {
         state.isAuth = false;
       }
-    }
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.isLoading = true;})
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isAuth = true;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isAuth = false;
+      });
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setEmail, setPassword, setIsLoading,setLogin } = userSlice.actions;
+export const { setEmail, setPassword, setIsLoading, setLogin } =
+  userSlice.actions;
 
 export default userSlice.reducer;
