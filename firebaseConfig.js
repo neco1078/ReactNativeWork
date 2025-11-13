aec52b0886afe519ee5d93257a7017c536f596c7
necmi <necmierturk@gmail.com>
2025-11-13 10:52:30 +0300

    crud operations

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getFirestore } from "firebase/firestore";
 import {
   FIREBASE_API_KEY,
   FIREBASE_AUTH_DOMAIN,
@@ -26,4 +27,6 @@ const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
+
+export const db = getFirestore(app);
 export default app;
--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,10 +1,57 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "../../firebaseConfig";
+import { CustomButton } from "../components";
+import React, { useState } from "react";
 const HomePage = () => {
+  const [data, setData] = useState([]);
+  console.log("getdata:", data);
+  const addData = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "reactNativeLesson"), {
+        title: "Zero to Hero",
+        content: "React native tutorial for beginners",
+        lesson: 95,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  const getData = async () => {
+    const querySnapshot = await getDocs(collection(db, "reactNativeLesson"));
+    querySnapshot.forEach((doc) => {
+      // console.log(`${doc.id} => ${doc.data()}`);
+      setData((prevData) => [...prevData, doc.data()]);
+    });
+  };
   return (
     <View style={styles.container}>
       <Text>HomePage</Text>
+      <Text>{data[0].title}</Text>
+      <Text>{data[1].title}</Text>
+
+      <CustomButton
+        buttonText="Save"
+        setWidth={"40%"}
+        buttonColor={"blue"}
+        pressedButtonColor={"gray"}
+        handleOnPress={addData}
+      />
+      <CustomButton
+        buttonText="getir"
+        setWidth={"40%"}
+        buttonColor={"blue"}
+        pressedButtonColor={"gray"}
+        handleOnPress={getData}
+      />
     </View>
   );
 };
@@ -16,5 +63,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "tomato",
   },
 });
